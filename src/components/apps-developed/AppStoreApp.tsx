3e6dd7a053d0c6327befe2278dac85ec9071933f
Daniel Cabrera <enamber01@gmail.com>
2026-03-09 15:11:24 +0100

    feat(appstore): add padding to AppStoreApp component for improved layout

--- src/components/apps-developed/AppStoreApp.tsx
+++ src/components/apps-developed/AppStoreApp.tsx
@@ -3,7 +3,7 @@ import AppStore from "@/components/apps/AppStore";
 
 const AppStoreApp = () => {
     return (
-        <div className="bg-transparent h-full min-h-0 w-full text-black dark:text-white flex flex-col">
+        <div className="bg-transparent h-full min-h-0 w-full text-black dark:text-white flex flex-col py-10">
             <AppStore />
         </div>
     );
